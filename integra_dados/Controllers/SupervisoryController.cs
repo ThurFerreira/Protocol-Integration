bd96154f667c709ce14e010cdbf384a32eea75e1
arthur <arthurfsilva110@gamial.com>
2025-06-11 12:38:27 -0300

    feat: Create http request

--- integra_dados/Controllers/SupervisoryController.cs
+++ integra_dados/Controllers/SupervisoryController.cs
@@ -8,14 +8,9 @@ namespace integra_dados.Controllers;
 public class SupervisoryController(SupervisoryService supervisoryService) : ControllerBase
 {
     [HttpPost("create")]
-    // OpenAPI/Swagger annotations via comments or Swashbuckle attributes (less verbose for common cases)
-    // [SwaggerOperation(Summary = "Cria um novo registro de supervisório para ser monitorado.", Description = "<<Não implementado ainda (Definir atributos do corpo)>>")]
-    // [SwaggerResponse(200, "Registro criado com sucesso", typeof(string))]
-    // [SwaggerResponse(409, "Registro já foi criado", typeof(string))]
-    // [SwaggerResponse(500, "Erro interno no servidor.", typeof(string))]
     public async Task<ActionResult<ResponseClient>> Create([FromBody] SupervisoryRegistry supervisory)
     {
-        supervisory.SetIdSistema(); // Assuming this method exists on SupervisoryRegistry
+        supervisory.SetIdSistema();
 
         var responseFromRegistry = await supervisoryService.Create(supervisory);
 
