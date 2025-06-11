2dfd324b05adf41599423efff3fb556e008da676
arthur <arthurfsilva110@gamial.com>
2025-06-11 12:38:15 -0300

    feat: FindByName implementation

--- integra_dados/Repository/SupervisoryRepository.cs
+++ integra_dados/Repository/SupervisoryRepository.cs
@@ -1,11 +1,20 @@
 using System.Reflection.Metadata;
+using integra_dados.Models;
+using MongoDB.Driver;
 
 namespace integra_dados.Repository;
 
-public class SupervisoryRepository() : ISupervisoryRepository
+public class SupervisoryRepository(IMongoCollection<SupervisoryRegistry> supervisoryRegistryCollection) : ISupervisoryRepository
 {
+
     public Task<Document> FindByName(string name)
     {
         throw new NotImplementedException();
+    }
+
+    public async Task<SupervisoryRegistry> Save(SupervisoryRegistry document)
+    {
+        await supervisoryRegistryCollection.InsertOneAsync(document);
+        return document;
     }
 }
