db4f5320142783e5db4257ad37953b6c041eae9e
arthur <arthurfsilva110@gamial.com>
2025-06-13 13:53:53 -0300

    feat: CORS config

--- integra_dados/Program.cs
+++ integra_dados/Program.cs
@@ -43,6 +43,16 @@ builder.WebHost.ConfigureKestrel(options =>
     options.ListenAnyIP(serverSettings.Port);
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()     // Permite qualquer origem
+            .AllowAnyMethod()     // Permite qualquer método HTTP
+            .AllowAnyHeader();    // Permite qualquer header
+    });
+});
+
 builder.Services.AddControllers();
 
 // Background Services
