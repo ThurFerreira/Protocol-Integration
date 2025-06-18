79b82541a965d21e302e41aa7b76c152755294a3
arthur <arthurfsilva110@gamial.com>
2025-06-18 17:11:49 -0300

    feat: FindOneByName

--- integra_dados/Repository/IRepository.cs
+++ integra_dados/Repository/IRepository.cs
@@ -5,7 +5,8 @@ namespace integra_dados.Repository;
 
 public interface IRepository<T>
 {
-    public Task<T> FindByName(String name);
+    public Task<List<T>> FindByName(String name);
+    public Task<T> FindOneByName(String name);
     public Task<T> Save(T document);
     public Task<T> FindById(string? idSistema);
     public Task<bool> DeleteById(string id);
