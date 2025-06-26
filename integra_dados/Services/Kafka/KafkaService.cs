b915de78d02df346d75499c2f6bd12131be2ffa2
arthur <arthurfsilva110@gamial.com>
2025-06-26 13:48:04 -0300

    feat: generic CreateBrokerPackage() for all registries

--- integra_dados/Services/Kafka/KafkaService.cs
+++ integra_dados/Services/Kafka/KafkaService.cs
@@ -25,7 +25,7 @@ public class KafkaService
         _producer = new ProducerBuilder<string, string>(config).Build();
     }
 
-    public Event1000_1 CreateBrokerPackage(ModbusRegistry registry, int reisterValue)
+    public Event1000_1 CreateBrokerPackage(Registry registry, int reisterValue)
     {
         return new Event1000_1(
             registry.Id,
@@ -36,7 +36,7 @@ public class KafkaService
         );
     }
     
-    public Event1000_1 CreateBrokerPackage(ModbusRegistry registry, bool reisterValue)
+    public Event1000_1 CreateBrokerPackage(Registry registry, bool reisterValue)
     {
         return new Event1000_1(
             registry.Id,
@@ -47,7 +47,7 @@ public class KafkaService
         );
     }
     
-    public Event1000_1 CreateBrokerPackage(ForecastRegistry registry, float[] reisterValue)
+    public Event1000_1 CreateBrokerPackage(Registry registry, float[] reisterValue)
     {
         return new Event1000_1(
             registry.Id,
@@ -58,7 +58,7 @@ public class KafkaService
         );
     }
     
-    public Event1000_1 CreateBrokerPackage(ForecastRegistry registry, float reisterValue)
+    public Event1000_1 CreateBrokerPackage(Registry registry, float reisterValue)
     {
         return new Event1000_1(
             registry.Id,
