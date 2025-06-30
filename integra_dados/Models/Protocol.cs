b74fd6bdd0f91c7e0b602cdb445d1bbe60ad08e1
arthur <arthurfsilva110@gamial.com>
2025-06-30 12:02:02 -0300

    fix: Protocol.cs as enum

--- integra_dados/Models/Protocol.cs
+++ integra_dados/Models/Protocol.cs
@@ -1,6 +1,17 @@
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
 namespace integra_dados.Models;
 
-public class Protocol
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum Protocol
 {
-    public string Name { get; set; }
+    //annotation for enum can be written as string
+    [EnumMember(Value = "dnp3")] dnp3,
+
+    [EnumMember(Value = "modbus")] modbus,
+
+    [EnumMember(Value = "opcua")] opcua,
+
+    [EnumMember(Value = "windy")] windy
 }
