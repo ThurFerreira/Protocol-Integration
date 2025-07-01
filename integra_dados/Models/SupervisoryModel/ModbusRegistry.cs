57a9adb579100473bc4769a40235465c294b14d3
arthur <arthurfsilva110@gamial.com>
2025-07-01 15:06:23 -0300

    feat: removing duplicated protocolo

--- integra_dados/Models/SupervisoryModel/ModbusRegistry.cs
+++ integra_dados/Models/SupervisoryModel/ModbusRegistry.cs
@@ -3,8 +3,6 @@ namespace integra_dados.Models;
 [BsonCollection("SupervisoryRegistry")]
 public class ModbusRegistry : Registry
 {
-    public string? Protocolo { get; set; }
-
     // Dados de monitoramento do supervisório
     public int EnderecoInicio { get; set; } //TODO mudar para int
     public int QuantidadeTags { get; set; }
